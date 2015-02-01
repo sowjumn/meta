7bf262a9e0ccd7ae6fcf64da7caf741eaf69eadb
sowjanya <sowju19@gmail.com>
2015-01-31 16:41:25 -0800

    better usage of class_eval and define_method

--- learn_meta.rb
+++ learn_meta.rb
@@ -1,10 +1,25 @@
 class LearnMeta
-  def self.run
-    parse = Class.new
-    parse.class_eval("def sowju; puts 'happy'; end")
+  attr_reader :parse
+
+  def initialize
+    @parse = Class.new
+  end
+
+  def run
+    method_n = 'sowju'
+    add_new_method(method_n)
     parse
+  end
+
+  def add_new_method(m_name)
+    parse.class_eval do 
+      p = Proc.new do 
+        puts "happy"
+      end
+      define_method(m_name,&p)
+    end
   end
 end
 
-u = LearnMeta.run
+u = LearnMeta.new.run
 u.new.sowju
